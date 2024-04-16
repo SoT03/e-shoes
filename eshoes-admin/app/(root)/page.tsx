19a73e2b31a8eabcaa49ca51cc1452c214a61a1d
SoT03 <bogdan.zygadlo123@gmail.com>
2024-04-16 16:08:03 +0200

    added working sign up

--- eshoes-admin/app/(root)/page.tsx
+++ eshoes-admin/app/(root)/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
-	return (
-		<div>
-			<Button variant={'outline'}>Click me</Button>
-		</div>
-	);
+export default function SetupPage() {
+	return <div>Not allowed</div>;
 }
