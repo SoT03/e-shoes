ec5ef8fc737c4688e54c098b8264390a1e30ec86
SoT03 <bogdan.zygadlo123@gmail.com>
2024-04-18 17:23:15 +0200

    made working create Store modal

--- eshoes-admin/app/(root)/page.tsx
+++ eshoes-admin/app/(root)/page.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import Modal from '@/components/ui/modal';
+import { useEffect } from 'react';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 export default function SetupPage() {
-	return (
-		<div className='p-4 '>
-			<Modal isOpen onClose={() => {}} title='Title' description='desc'>
-				Children
-			</Modal>
-		</div>
-	);
+	const onOpen = useStoreModal((state) => state.onOpen);
+	const isOpen = useStoreModal((state) => state.isOpen);
+
+	useEffect(() => {
+		if (!isOpen) {
+			onOpen();
+		}
+	}, [isOpen, onOpen]);
+
+	return <div className='p-4 '>Root Page</div>;
 }
