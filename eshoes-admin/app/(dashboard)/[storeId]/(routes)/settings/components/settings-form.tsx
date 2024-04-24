6e5927c8fa4137de9ad58cbf7594a3a4365d4ee6
SoT03 <bogdan.zygadlo123@gmail.com>
2024-04-24 16:24:47 +0200

    added form schema for settigns page

--- eshoes-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ eshoes-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,16 +1,39 @@
 'use client';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
 import Heading from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
 import { Store } from '@prisma/client';
+import { Trash } from 'lucide-react';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 interface SettingsFormProps {
 	initialData: Store;
 }
 
+const formSchema = z.object({
+	name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 export default function SettingsForm({ initialData }: SettingsFormProps) {
+	const form = useForm<SettingsFormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: initialData,
+	});
+
 	return (
-		<div className='flex items-center justify-between'>
-			<Heading title='Settings' description="Manage store preferences"  />
-		</div>
+		<>
+			<div className='flex items-center justify-between'>
+				<Heading title='Settings' description='Manage store preferences' />
+				<Button variant='destructive' size='icon' onClick={() => {}}>
+					<Trash className='h-4 w-4' />
+				</Button>
+			</div>
+			<Separator />
+		</>
 	);
 }
