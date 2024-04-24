e2e1f685e20e2d88c4cdeb3919e89f1fbcb21e09
SoT03 <bogdan.zygadlo123@gmail.com>
2024-04-24 16:36:45 +0200

    created form structure

--- eshoes-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ eshoes-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -5,9 +5,17 @@ import Heading from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { Store } from '@prisma/client';
 import { Trash } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 interface SettingsFormProps {
 	initialData: Store;
@@ -20,10 +28,17 @@ const formSchema = z.object({
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 export default function SettingsForm({ initialData }: SettingsFormProps) {
+	const [open, setOpen] = useState(false);
+	const [loading, setLoading] = useState(false);
+
 	const form = useForm<SettingsFormValues>({
 		resolver: zodResolver(formSchema),
 		defaultValues: initialData,
 	});
+
+	const onSubmit = async (data: SettingsFormValues) => {
+		console.log(data);
+	};
 
 	return (
 		<>
@@ -34,6 +49,30 @@ export default function SettingsForm({ initialData }: SettingsFormProps) {
 				</Button>
 			</div>
 			<Separator />
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='space-y-8 w-full'>
+					<div className='grid grid-cols-3 gap-8'>
+						<FormField
+							control={form.control}
+							name='name'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input
+											disabled={loading}
+											placeholder='Store name'
+											{...field}
+										/>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+					</div>
+				</form>
+			</Form>
 		</>
 	);
 }
