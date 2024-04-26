ff93f7a91dd96e84415e48994a267c7bac265fcc
SoT03 <bogdan.zygadlo123@gmail.com>
2024-04-26 21:52:03 +0200

    added billboards page

--- eshoes-admin/components/main-nav.tsx
+++ eshoes-admin/components/main-nav.tsx
@@ -13,14 +13,19 @@ export default function MainNav({
 
 	const routes = [
 		{
-			href: `/${params.storeId}/settings`,
-			label: 'Settings',
-			active: pathname === `/${params.storeId}/settings`,
-		},
-		{
 			href: `/${params.storeId}`,
 			label: 'Overview',
 			active: pathname === `/${params.storeId}`,
+		},
+		{
+			href: `/${params.storeId}/billboards`,
+			label: 'Billboards',
+			active: pathname === `/${params.storeId}/billboards`,
+		},
+		{
+			href: `/${params.storeId}/settings`,
+			label: 'Settings',
+			active: pathname === `/${params.storeId}/settings`,
 		},
 	];
 
