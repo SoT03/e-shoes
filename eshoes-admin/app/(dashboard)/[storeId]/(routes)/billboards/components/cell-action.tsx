3a2faa0cd878ab7c25a3a42a54b8d1da86c6d327
SoT03 <bogdan.zygadlo123@gmail.com>
2024-05-07 19:25:42 +0200

    added dropdown menu for actions

--- eshoes-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ eshoes-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import { BillboardColumn } from './columns';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react';
 
-export default function cellAction() {
-  return (
-    <div>cell-action</div>
-  )
+interface CellActionProps {
+	data: BillboardColumn;
+}
+
+export default function CellAction({ data }: CellActionProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant='ghost' className='h-8 w-8 p-0'>
+					<span className='sr-only'>Open menu</span>
+					<MoreHorizontal className='h-4 w-4 ' />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align='end'>
+				<DropdownMenuLabel>Actions</DropdownMenuLabel>
+				<DropdownMenuItem>
+					<Copy className='mr-2 h-4 w-4' />
+					Copy Id
+				</DropdownMenuItem>
+				<DropdownMenuItem>
+					<Edit className='mr-2 h-4 w-4' />
+					Update
+				</DropdownMenuItem>
+				<DropdownMenuItem>
+					<Trash className='mr-2 h-4 w-4' />
+					Delete
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
