805051ec0093341647968c3dbbc17b04ef83ddb2
SoT03 <bogdan.zygadlo123@gmail.com>
2024-05-08 19:14:08 +0200

    created categories page

--- eshoes-admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ eshoes-admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -3,7 +3,7 @@ import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import Heading from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Billboard } from '@prisma/client';
+import { Category } from '@prisma/client';
 import { Trash } from 'lucide-react';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -22,40 +22,38 @@ import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import AlertModal from '@/components/modals/alert-modal';
 
-import ImageUpload from '@/components/ui/image-upload';
-
-interface BillboardFormProps {
-	initialData: Billboard | null;
+interface CategoryFormProps {
+	initialData: Category | null;
 }
 
 const formSchema = z.object({
-	label: z.string().min(1),
-	imageUrl: z.string().min(1),
+	name: z.string().min(1),
+	billboardId: z.string().min(1),
 });
 
-type BillboardFormValues = z.infer<typeof formSchema>;
+type CategoryFormValues = z.infer<typeof formSchema>;
 
-export default function BillboardForm({ initialData }: BillboardFormProps) {
+export default function CategoryForm({ initialData }: CategoryFormProps) {
 	const [open, setOpen] = useState(false);
 	const [loading, setLoading] = useState(false);
 
 	const params = useParams();
 	const router = useRouter();
 
-	const title = initialData ? 'Edit billboard' : 'Create billboard';
-	const description = initialData ? 'Edit a billboard' : 'Add a new billboard';
-	const toastMessage = initialData ? 'Billboard updated' : 'Billboard created.';
+	const title = initialData ? 'Edit category' : 'Create category';
+	const description = initialData ? 'Edit a category' : 'Add a new category';
+	const toastMessage = initialData ? 'Category updated' : 'Category created.';
 	const action = initialData ? 'Save changes' : 'Create';
 
-	const form = useForm<BillboardFormValues>({
+	const form = useForm<CategoryFormValues>({
 		resolver: zodResolver(formSchema),
 		defaultValues: initialData || {
-			label: '',
-			imageUrl: '',
+			name: '',
+			billboardId: '',
 		},
 	});
 
-	const onSubmit = async (data: BillboardFormValues) => {
+	const onSubmit = async (data: CategoryFormValues) => {
 		try {
 			setLoading(true);
 			if (initialData) {
@@ -67,7 +65,6 @@ export default function BillboardForm({ initialData }: BillboardFormProps) {
 				await axios.post(`/api/${params.storeId}/billboards`, data);
 			}
 			router.refresh();
-			router.push(`/${params.storeId}/billboards`);
 			toast.success(toastMessage);
 		} catch (error) {
 			toast.error('Something went wrong');
@@ -121,35 +118,17 @@ export default function BillboardForm({ initialData }: BillboardFormProps) {
 				<form
 					onSubmit={form.handleSubmit(onSubmit)}
 					className='space-y-8 w-full'>
-					<FormField
-						control={form.control}
-						name='imageUrl'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Background image</FormLabel>
-								<FormControl>
-									<ImageUpload
-										value={field.value ? [field.value] : []}
-										disabled={loading}
-										onChange={(url) => field.onChange(url)}
-										onRemove={() => field.onChange('')}
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
 					<div className='grid grid-cols-3 gap-8'>
 						<FormField
 							control={form.control}
-							name='label'
+							name='name'
 							render={({ field }) => (
 								<FormItem>
-									<FormLabel>Label</FormLabel>
+									<FormLabel>Name</FormLabel>
 									<FormControl>
 										<Input
 											disabled={loading}
-											placeholder='Billboard label'
+											placeholder='Category name'
 											{...field}
 										/>
 									</FormControl>
