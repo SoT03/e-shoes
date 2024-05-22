612acc9de78de5d5c7daa4df7614e1f2009a02f4
SoT03 <bogdan.zygadlo123@gmail.com>
2024-05-22 18:55:34 +0200

    finished category screen

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -6,6 +6,9 @@ import Billboard from '@/components/billboard';
 import Container from '@/components/ui/container';
 import React from 'react';
 import Filter from './components/filter';
+import NoResults from '@/components/ui/no-results';
+import ProductCard from '@/components/ui/product-card';
+import MobileFilters from './components/mobile-filter';
 export const revalidate = 0;
 
 interface CategoryPageProps {
@@ -37,9 +40,18 @@ export default async function CategoryPage({
 				<Billboard data={category.billboard} />
 				<div className='px-4 sm:px-6 md:px-8 pb-24'>
 					<div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
-						{/* Mobile filters here */}
+						<MobileFilters sizes={sizes} colors={colors} />
 						<div className='hidden lg:block'>
 							<Filter valueKey='sizeId' name='Sizes' data={sizes} />
+							<Filter valueKey='colorId' name='Colors' data={colors} />
+						</div>
+						<div className='mt-6 lg:col-span-4 lg:mt-0'>
+							{products.length === 0 && <NoResults />}
+							<div className='grid grid-cols sm:grid-cols-2 md:grid-cols-3 gap-4'>
+								{products.map((item) => {
+									return <ProductCard key={item.id} data={item} />;
+								})}
+							</div>
 						</div>
 					</div>
 				</div>
