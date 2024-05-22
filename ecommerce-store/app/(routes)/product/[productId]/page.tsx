3483be8675e45c3d9c07bf29003cb678ddafdeee
SoT03 <bogdan.zygadlo123@gmail.com>
2024-05-22 17:20:35 +0200

    created gallery component

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,7 @@
 import getProduct from '@/actions/get-product';
 import getProducts from '@/actions/get-products';
+import Gallery from '@/components/gallery';
+import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 import React from 'react';
 
@@ -20,9 +22,11 @@ export default async function ProductPage({ params }: ProductPageProps) {
 			<Container>
 				<div className='px-4 py-10 sm:px-6 lg:px-8'>
 					<div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
-						<div>Gallery</div>
+						<Gallery images={product.images} />
 						<div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>Info</div>
 					</div>
+					<hr className='my-10' />
+					<ProductList title='Related items' items={suggestedProducts} />
 				</div>
 			</Container>
 		</div>
