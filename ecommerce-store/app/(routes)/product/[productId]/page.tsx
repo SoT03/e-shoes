d8d61a05a43e96e3d5ce2a7eb35ac5eb54dcde95
SoT03 <bogdan.zygadlo123@gmail.com>
2024-05-22 18:02:15 +0200

    finished indiovidual item page

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,8 @@ import getProducts from '@/actions/get-products';
 import Gallery from '@/components/gallery';
 import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
+import Info from '@/components/ui/info';
+
 import React from 'react';
 
 interface ProductPageProps {
@@ -23,7 +25,9 @@ export default async function ProductPage({ params }: ProductPageProps) {
 				<div className='px-4 py-10 sm:px-6 lg:px-8'>
 					<div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
 						<Gallery images={product.images} />
-						<div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>Info</div>
+						<div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
+							<Info data={product} />
+						</div>
 					</div>
 					<hr className='my-10' />
 					<ProductList title='Related items' items={suggestedProducts} />
