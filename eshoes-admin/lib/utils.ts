ad3695900a032e9fa17634e204917ebdf8fa73d2
SoT03 <bogdan.zygadlo123@gmail.com>
2024-05-28 22:32:39 +0200

    added basic look for overview page

--- eshoes-admin/lib/utils.ts
+++ eshoes-admin/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-export const foramtter = new Intl.NumberFormat('en-US', {
+export const formatter = new Intl.NumberFormat('en-US', {
 	style: 'currency',
 	currency: 'USD',
 });
