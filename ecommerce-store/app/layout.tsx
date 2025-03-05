2574444240370a56497e2ad64894d4af9aeabb70
SoT03 <bogdan.zygadlo123@gmail.com>
2025-03-06 00:40:18 +0100

    added loading suspense

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import ModalProvider from '@/providers/modal-provider';
 import ToastProvider from '@/providers/toast-provider';
+import { Suspense } from 'react';
 
 const font = Urbanist({ subsets: ['latin'] });
 
@@ -21,11 +22,13 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={font.className}>
-				<ModalProvider />
-				<ToastProvider />
-				<Navbar />
-				{children}
-				<Footer />
+				<Suspense fallback={<>Loading...</>}>
+					<ModalProvider />
+					<ToastProvider />
+					<Navbar />
+					{children}
+					<Footer />
+				</Suspense>
 			</body>
 		</html>
 	);
